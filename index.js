1cd30638f512e87c01b2097c073e71a2d3fae995
Ian <ianmcm.89@gmail.com>
2021-09-08 02:13:01 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
+  return fetch("https://anapioficeandfire.com/api/books");
 }
 
 function renderBooks(books) {
@@ -15,3 +18,29 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+/*  Deliveries:
+
+function fetchBooks() {
+  // Return the 5th book in the series
+  fetch("https://anapioficeandfire.com/api/books/5")
+    .then((resp) => resp.json())
+    .then((json) => console.log(json));
+  
+  // Returns the 1031st character in the series
+  fetch("https://anapioficeandfire.com/api/characters/1031")
+    .then((resp) => resp.json())
+    .then((json) => console.log(json));
+
+  // Returns the total number of pages of all the books
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => {
+      let totalPages = 0;
+      json.forEach(book => {
+      totalPages+= book.numberOfPages;
+      });
+      console.log(totalPages);
+    });
+}
+*/
